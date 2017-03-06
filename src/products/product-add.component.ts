95583fdeb97fb8cf37e4d28a9472243ca62c2ef6
Praveen Raj <praveensengodan@gmail.com>
2017-03-06 20:49:02 +0530

    Product value taken from Form instead of model

--- src/products/product-add.component.ts
+++ src/products/product-add.component.ts
@@ -1,6 +1,7 @@
 // Angular
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
+import { NgForm } from '@angular/forms';
 
 // Service
 import { ProductService } from './product.service';
@@ -15,14 +16,17 @@ import { IProduct } from './product';
   template: require('./product-add.component.html')
 })
 export class AddProductComponent {
- constructor(private _productService: ProductService, private _router: Router){}
+ constructor(
+  private _productService: ProductService,
+  private _router: Router
+  ){}
  pageTitle: string = 'Add New Product';
  product: IProduct = new Product();
  onBack(): void {
    this._router.navigate(['./product']);
  }
- onFormSubmit(form): void {
-   const {productName,price,image,rating} = this.product;
+ onFormSubmit(form: NgForm): void {
+   const {productName,price,image,rating} = form.value;
    this._productService.saveProduct({productName,price,image,rating}).subscribe(()=>{
     alert('Product Saved Successfully');
     this._router.navigate(['./product']);
