b159e2cd06ca9fba751dd51d8f04500b3d534207
Praveen Raj <praveensengodan@gmail.com>
2017-03-06 19:23:12 +0530

    Added form validation and Product Model

--- src/products/product-add.component.ts
+++ src/products/product-add.component.ts
@@ -5,21 +5,24 @@ import { Router } from '@angular/router';
 // Service
 import { ProductService } from './product.service';
 
+// Model
+import { Product } from './product.model';
+
+// Interface
+import { IProduct } from './product';
+
 @Component({
   template: require('./product-add.component.html')
 })
 export class AddProductComponent {
  constructor(private _productService: ProductService, private _router: Router){}
  pageTitle: string = 'Add New Product';
- productName: string;
- price: number;
- image: string;
- rating: string;
+ product: IProduct = new Product();
  onBack(): void {
    this._router.navigate(['./product']);
  }
  onFormSubmit(form): void {
-   const {productName,price,image,rating} = this;
+   const {productName,price,image,rating} = this.product;
    this._productService.saveProduct({productName,price,image,rating}).subscribe(()=>{
     alert('Product Saved Successfully');
     this._router.navigate(['./product']);
---
+++ src/products/product.model.ts
@@ -0,0 +1,12 @@
+export class Product {
+  productName: string;
+  price: number;
+  image: string;
+  rating: number;
+  constructor() {
+   this.productName;
+   this.price;
+   this.image;
+   this.rating;
+  }
+}
